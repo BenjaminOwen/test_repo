85df5916086b5e41e644c596f5fa5c256fbb72ab
unknown <o006189c@k106-1.student.staffs.ac.uk>
2014-10-01 11:29:56 +0100

    Added Player Class

---
+++ my_first_repo/my_first_repo/my_first_repo/my_first_repo/Player.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace my_first_repo
+{
+	public class Player
+	{
+						
+			public int health;
+			public int power;
+			public int armor;
+			public int speed;
+			public int xPos;
+			public int yPos;	
+			
+		public Player ()
+		{
+			
+		}
+		
+		
+		public void GetHealth ()
+		
+		{
+			return health;
+		}
+		
+		public void GetPower ()
+		
+		{
+			return power;
+		}
+		
+		public void GetArmor ()
+		
+		{
+			return armor;
+		}
+		
+		public void GetSpeed ()
+		
+		{
+			return speed;
+		}
+		
+		public void GetXPos ()
+		
+		{
+			return xPos;
+		}
+		
+		public void GetYPos ()
+		
+		{
+			return yPos;
+		}
+		
+	}
+}
+
